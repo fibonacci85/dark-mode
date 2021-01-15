010b1828291c5a4031318c1fcf68bad239fd97f2
Francis Bonacic <francisbonacic@icloud.com>
2021-01-14 21:07:32 -0800

    finished

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import useLocalStorage from "./useLocalStorage"
+
+const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage('key');
+
+    return [darkMode, setDarkMode]
+}
+
+
+
+export default useDarkMode 
--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,22 +1,17 @@
-import { useState } from "react";
+import { useState } from "react"
 
 const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key)
+        return item ? JSON.parse(item) : initialValue
+    })
 
-    const [storedValue, setStoredValue] = useState(()=>{
-        if (localStorage.getItem(key)) {
-            return JSON.parse(localStorage.getItem(key));
-        }
-
-        localStorage.setItem(key, JSON.stringify(initialValue));
-        return initialValue;
-    });
-
-    const setValue = value => {
-        setStoredValue(value);
-        localStorage.setItem(key, JSON.stringify(value));
+    const setValue = (value) => {
+        setStoredValue(value)
+        window.localStorage.setItem(key, JSON.stringify(value))
     }
 
-    return [storedValue, setValue];
+    return [storedValue, setValue]
 }
 
-export default useLocalStorage;
+export default useLocalStorage 
